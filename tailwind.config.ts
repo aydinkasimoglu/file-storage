e4e4985876562dfd1e430ce09d761f4e71665785
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-12-10 20:03:00 +0300

    Style variables and stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          hover: 'hsl(var(--primary-hover))',
+          disabled: 'hsl(var(--primary-disabled))',
+        },
+        input: {
+          DEFAULT: 'hsl(var(--input))',
+          hover: 'hsl(var(--input-hover))',
+        },
+        'transparent-hover': 'rgba(var(--transparent-hover))',
+      },
+      minHeight: {
+        screen: "100dvh",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
