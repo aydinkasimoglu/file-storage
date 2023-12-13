76af65c5444d3784d04aba3e9ba33d45d7e8967c
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-12-14 00:01:45 +0300

    Changed config for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+      },
+      {
+        protocol: "http",
+        hostname: "**.amazonaws.com"
+      }
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
